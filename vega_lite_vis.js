c8c9be896b9de383e10d6cefcd3d5d153830a97d
Elee0068 <145523249+Elee0068@users.noreply.github.com>
2023-10-08 13:04:48 +0800

    Uploading js and html

---
+++ vega_lite_vis.js
@@ -0,0 +1,11 @@
+var vg_1 = "WineImports.vg.json"; 
+var vg_2 = "pie.vg.json"; 
+
+vegaEmbed("#map", vg_1).then(function(result) {
+    // Access the Vega view instance 
+    //(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#pie", vg_2).then(function(result) {
+    // Access the Vega view instance 
+    //(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
